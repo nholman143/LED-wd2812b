0d37bec252cb63f03d7e1bb1a7433d024dd038b3
nholman143 <64487565+nholman143@users.noreply.github.com>
2020-09-29 17:42:11 -0500

    Update Flash.py

--- Flash.py
+++ Flash.py
@@ -1,3 +1,7 @@
+"""
+Two Color Flash
+"""
+
 import board
 import time
 import neopixel
@@ -8,6 +12,8 @@ pixels = neopixel.NeoPixel(board.D18, 120)
 #Simple loop
 while True:
 #.fill to light up all the leds
+#Change (x, x, x) for diffrent colors
+#Add more colors to the loop by makeing another .fill line
     pixels.fill((0, 0, 255))
     time.sleep(1)
     pixels.fill((255, 0, 0))
